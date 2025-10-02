b00ed266541b1631818394872aec83c4989c5a0b
siddharth <siddharth@siddu.iitbhilai.ac.in>
2025-10-02 14:52:38 +0530

    final

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_DB_URL).then(() => {
   console.log('connected to DB');
-  app.listen(6969,'0.0.0.0',()=>{
-      console.log(`app is listening on port ${process.env.PORT}`);
-  })
-  // app.listen(process.env.PORT, () => {
-  //   console.log(`app is listening on port ${process.env.PORT}`);
-  // });
+  // app.listen(6969,'0.0.0.0',()=>{
+  //     console.log(`app is listening on port ${process.env.PORT}`);
+  // })
+  app.listen(process.env.PORT, () => {
+    console.log(`app is listening on port ${process.env.PORT}`);
+  });
 });
 
 
