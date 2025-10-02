f154af7f9b71bddf3edebae1e9734cc867aad1ca
siddharth <siddharth@siddu.iitbhilai.ac.in>
2025-10-02 14:27:01 +0530

    final

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../config/yi-whats-on-firebase-adminsdk-fbsvc-2242d46944.json');
+const serviceAccount = require('./config/yi-whats-on-firebase-adminsdk-fbsvc-2242d46944.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
