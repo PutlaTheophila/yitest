e51ad3d1edb9ca1619a1043639bcf8b548b75738
siddharth <siddharth@siddu.iitbhilai.ac.in>
2025-10-02 15:05:38 +0530

    final

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -8,22 +8,17 @@
 // module.exports = admin;
 
 
+require('dotenv').config(); // <- must be first
 
-const fs = require('fs');
-const path = require('path');
 const admin = require('firebase-admin');
-require('dotenv').config();
 
-// Path where we'll temporarily write the secret inside the container
-const serviceAccountPath = path.join(__dirname, 'serviceAccountKey.json');
+const serviceAccountJSON = process.env.GOOGLE_APPLICATION_CREDENTIALS_B64;
+if (!serviceAccountJSON) {
+  throw new Error('GOOGLE_APPLICATION_CREDENTIALS_B64 is not defined');
+}
 
-// Write the Firebase JSON from the environment variable at runtime
-fs.writeFileSync(serviceAccountPath, process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountJSON, 'base64').toString('utf8'));
 
-// Load the service account from the temporary file
-const serviceAccount = require(serviceAccountPath);
-
-// Initialize Firebase
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
